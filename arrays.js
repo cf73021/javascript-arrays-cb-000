8950df35706979a12ee1ae5d79f33ea4f70da0de
Chris Franklin <cf73021@gmail.com>
2019-01-25 22:22:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function sdestructivelyRemoveElementToBeginningOfArray(array) {
+function destructivelyRemoveElementToBeginningOfArray(array) {
   array.shift()
   return array
 }
