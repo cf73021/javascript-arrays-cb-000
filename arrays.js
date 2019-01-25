ba19a5e028815f7a5086d48fd7eacb3a18d9b1e8
Chris Franklin <cf73021@gmail.com>
2019-01-25 22:20:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+destructivelyAddElementToBeginningOfArray(array) {
+  
+}
